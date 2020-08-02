5616689d93e51326db04348cceb3ecfe1c86bbe4
Andrew Raines <rainesand26@gmail.com>
2020-08-01 22:49:31 -0400

    added functionality with heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const mainDir = path.join(__dirname,'/public');
 //EXPRESS section
 app.use(express.json());
